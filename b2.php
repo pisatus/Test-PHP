7143766b695b42738291b2706f4692f629b9891f
Nuno Rocha <63843004+pisatus@users.noreply.github.com>
2024-02-08 17:23:56 +0000

    aaa

--- b2.php
+++ b2.php
@@ -1,14 +1,14 @@
 <?php
-echo"1231231232"
+    echo"1231231232"
 
-$aaa = $_SESSION['func_name']($_GET['argument']);
+    $aaa = $_SESSION['func_name']($_GET['argument']);
 
-$y = str_replace('z', 'e', 'zxzc');
-$y("malicious code");
+    $y = str_replace('z', 'e', 'zxzc');
+    $y("malicious code");
 
-assert('ex' . 'ec("kill --bill")');
+    assert('ex' . 'ec("kill --bill")');
 
-/// note: proof of principle code, don't use
-$include = $_GET['file'];
-if ( preg_match("/\\.php$/",$include) ) include($include);
+    /// note: proof of principle code, don't use
+    $include = $_GET['file'];
+    if ( preg_match("/\\.php$/",$include) ) include($include);
 ?>
